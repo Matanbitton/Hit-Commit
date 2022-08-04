26047bf648921d4bfb02cb832287d23d52475618
Matan <matanbitton2@gmail.com>
2022-08-04 12:43:16 +0300

    make nav UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,13 +3,18 @@ import NavButtons from "./NavButtons";
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="navigation">
       <img className="logo" src="" alt=""></img>
-      <input placeholder="Search"></input>
-      <NavButtons />
-      <p className="bell">bell icon</p>
-      <p className="bell">plus icon</p>
-      <img className="profile-pic"></img>
+      <div className="temp">
+        <input placeholder="Search"></input>
+        <NavButtons />
+      </div>
+
+      <div className="icons">
+        <p className="bell">bell icon</p>
+        <p className="bell">plus icon</p>
+      </div>
+      <img className="profile-pic" src="" alt=""></img>
     </nav>
   );
 }
