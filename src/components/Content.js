f6b9f3762a5b875950fc4af505ca678057fddd02
Matan <matanbitton2@gmail.com>
2022-08-07 20:09:11 +0300

    remove unused icon

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,11 +5,14 @@ import { faBullseye, faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 import format from "date-fns/format";
 import commitMessages from "./commitMessages";
 import { useState } from "react";
+import Footer from "./Footer";
 
 export default function Content() {
   const date = format(new Date(), " MMM d ,yyyy");
 
-  const [commitMessage, setCommitMessage] = useState();
+  const [commitMessage, setCommitMessage] = useState(
+    commitMessages.commits[77]
+  );
 
   function getCommitMessage() {
     const randomNum = Math.floor(Math.random() * commitMessages.commits.length);
@@ -18,15 +21,18 @@ export default function Content() {
   }
 
   return (
-    <div className="main-content">
-      <RepoButton
-        title="main ▼"
-        icon={<FontAwesomeIcon icon={faCodeBranch} />}
-      />
-      <p className="date">Commits on {date}</p>
-      <p className="line"></p>
-      <p className="line-icon">{<FontAwesomeIcon icon={faBullseye} />}</p>
-      <Commit text={commitMessage} handleClick={getCommitMessage} />
+    <div>
+      <div className="main-content">
+        <RepoButton
+          title="main ▼"
+          icon={<FontAwesomeIcon icon={faCodeBranch} />}
+        />
+        <p className="date">Commits on {date}</p>
+
+        <p className="line"></p>
+        <Commit text={commitMessage} handleClick={getCommitMessage} />
+      </div>
+      <Footer />
     </div>
   );
 }
